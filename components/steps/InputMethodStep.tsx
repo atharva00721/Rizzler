95b315bcf7e7cbc220ed4a1fe5cdaf2a9a72cfcc
Atharva <atharvaraj@gmail.com>
2025-02-19 04:57:47 +0530

    ok

--- components/steps/InputMethodStep.tsx
+++ components/steps/InputMethodStep.tsx
@@ -6,7 +6,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+
 
 interface InputMethodStepProps {
   onSelectMethod: (method: "text" | "image") => void;
